560e7956eee43ed628d7793157b375d9171fa24f
hourin <phucjaki@gmail.com>
2026-05-10 18:06:54 +0700

    Added: automatically inject project name to Scala code

--- ner.py
+++ ner.py
@@ -1,10 +1,10 @@
 from cpgqls_client import CPGQLSClient, import_code_query, workspace_query;
 import argparse;
+from string import Template;
+from pathlib import Path;
 
-def clean_joern_output(raw_str):
-    import re;
-    cleaned = re.sub(r'val res\d+:.*', '', raw_str).strip();
-    return cleaned;
+def guess_project_name(path):
+    return Path(path).name;
 
 def main(query, args):
     result = client.execute(import_code_query(args.s));
@@ -12,7 +12,7 @@ def main(query, args):
 
     result = client.execute(query);
     with open("joern.json", "w") as file:
-        output = clean_joern_output(result['stdout']);
+        output = result['stdout'];
         file.write(output);
 
 if __name__ == "__main__":
@@ -24,6 +24,8 @@ if __name__ == "__main__":
     client = CPGQLSClient(server);
 
     with open("ner.sc", "r") as file:
-        query = file.read();
+        output = guess_project_name(args.s);
+        query = Template(file.read());
+        query = query.substitute(output=output);
         main(query, args);
 
--- ner.sc
+++ ner.sc
@@ -1,2 +1,2 @@
-print("test")
+print("running on $output")
 
